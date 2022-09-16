d3bfb7bd3a21696f041a31b90d3f4b5f9329fc97
Tim Stacey <tim.j.stacey@gmail.com>
2022-09-17 09:28:08 +1000

    switch reporter

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+![Cypress Tests](https://github.com/github/docs/actions/workflows/main.yml/badge.svg)
+
 # React ToDo MVC Cypress tests
 This repostitory uses the pulically available [Playwright React ToDo](https://demo.playwright.dev/todomvc) to showcase cypress e2e tests.
 
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -14,7 +14,15 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands'
+import './commands';
+import addContext from 'mochawesome/addContext';
+
+Cypress.on('test:after:run', (test, runnable) => {
+  if (test.state === 'failed') {
+    const screenshot = `assets/${Cypress.spec.name}/${runnable.parent.title} -- ${test.title} (failed).png`;
+    addContext({ test }, screenshot);
+  }
+});
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
